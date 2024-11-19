645848c4137ca926ef661da5ee8858439befd61f
Kenn1563 <237015@himejo.jp>
2024-11-19 16:24:46 +0900

    画面遷移
    タイトル画面→名前入力→キャラクター選択
    になるようにしました。

--- Assets/Scenes/Scrips/StartButton.cs
+++ Assets/Scenes/Scrips/StartButton.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartBotton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string SceneName;
+
+
     void Start()
     {
-        
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneName);
+        });
     }
+
 
     // Update is called once per frame
     void Update()
