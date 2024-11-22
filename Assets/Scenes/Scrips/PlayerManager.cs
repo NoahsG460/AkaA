607c40444d7542a3e9dd3b7539693967ded94799
NoahsG460 <xinyihouteng729@gmail.com>
2024-11-22 11:19:24 +0900

    分けた

--- Assets/Scenes/Scrips/PlayerManager.cs
+++ Assets/Scenes/Scrips/PlayerManager.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public float moveSpeed = 3f;
-    public float boostedSpeed = 6f; // シフトキーで速くなる速度
-    public float jumpForce = 5f; // ジャンプ力を設定
     public Transform attackPoint;
     public float attackRadius; // 攻撃の半径
     public LayerMask enemyLayer;
-    Rigidbody2D rb;
     Animator animator;
     public int hp = 5; // プレイヤーのHPを設定
     int attackPower = 1;
-    private bool isGrounded; // 地面に接地しているかの判定
-    private bool isBoosting; // スピードアップ中かを判定
-    private float currentSpeed; // 現在の移動速度
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -31,47 +23,10 @@ public class PlayerManager : MonoBehaviour
             Attack();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<PlayerStamina>().IsGrounded)
         {
             Jump();
-            Debug.Log("ジャンプがトリガーされました");
         }
-
-        // シフトキーでスピードアップ（地上にいるときのみ）
-        if (isGrounded && Input.GetKey(KeyCode.LeftShift))
-        {
-            isBoosting = true;
-        }
-        else if (isGrounded)
-        {
-            isBoosting = false;
-        }
-
-        // プレイヤーの移動
-        Movement();
-    }
-
-    void Movement()
-    {
-        float x = Input.GetAxisRaw("Horizontal"); // 横方向の入力 (A/Dキーや矢印キー)
-
-        // 現在の速度を計算（地上でのみブースト適用）
-        currentSpeed = isBoosting ? boostedSpeed : moveSpeed;
-
-        // 向きの変更
-        if (x > 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (x < 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-
-        animator.SetFloat("Speed", Mathf.Abs(x)); // アニメーションのスピード設定
-
-        // 移動を適用
-        rb.velocity = new Vector2(x * currentSpeed, rb.velocity.y);
     }
 
     void Attack()
@@ -90,8 +45,8 @@ public class PlayerManager : MonoBehaviour
 
     void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        isGrounded = false;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<PlayerStamina>().JumpForce);
+        GetComponent<PlayerStamina>().IsGrounded = false;
     }
 
     public void OnDamage(int damage)
@@ -125,7 +80,7 @@ public class PlayerManager : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true; // 地面に接地
+            GetComponent<PlayerStamina>().IsGrounded = true; // 地面に接地
         }
     }
 
--- Assets/Scenes/Scrips/PlayerStamina.cs
+++ Assets/Scenes/Scrips/PlayerStamina.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerStamina : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 3f;
+    public float boostedSpeed = 6f; // シフトキーで速くなる速度
+    public float jumpForce = 5f; // ジャンプ力を設定
+    public bool IsGrounded { get; set; } // 地面に接地しているかの判定
+    private bool isBoosting; // スピードアップ中かを判定
+    private float currentSpeed; // 現在の移動速度
+    Rigidbody2D rb;
+    Animator animator;
+
+    public float JumpForce => jumpForce; // ジャンプ力を公開
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // シフトキーでスピードアップ（地上にいるときのみ）
+        if (IsGrounded && Input.GetKey(KeyCode.LeftShift))
+        {
+            isBoosting = true;
+        }
+        else if (IsGrounded)
+        {
+            isBoosting = false;
+        }
+
+        // プレイヤーの移動
+        Movement();
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); // 横方向の入力 (A/Dキーや矢印キー)
+
+        // 現在の速度を計算（地上でのみブースト適用）
+        currentSpeed = isBoosting ? boostedSpeed : moveSpeed;
+
+        // 向きの変更
+        if (x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(x)); // アニメーションのスピード設定
+
+        // 移動を適用
+        rb.velocity = new Vector2(x * currentSpeed, rb.velocity.y);
     }
 }
