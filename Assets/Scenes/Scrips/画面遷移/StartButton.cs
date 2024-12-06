03e182bda7962823e2718f37d04e865396f82da9
NoahsG460 <xinyihouteng729@gmail.com>
2024-12-06 10:06:29 +0900

    ボタン透過

--- Assets/Scenes/Scrips/画面遷移/StartButton.cs
+++ Assets/Scenes/Scrips/画面遷移/StartButton.cs
@@ -1,26 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartBotton : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
-    public string SceneName;
+    public Image buttonImage;  // ボタンのImageコンポーネントを指定
+    public float blinkSpeed = 1.0f;  // 点滅速度
 
+    private bool isIncreasing = true;  // 透明度が増加中か減少中か
+    private float alpha = 1.0f;        // 透明度
 
-    void Start()
-    {
-        GetComponent<Button>().onClick.AddListener(() =>
-        {
-            SceneManager.LoadScene("名前入力画面");
-        });
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (buttonImage != null)
+        {
+            // 点滅の透明度計算
+            if (isIncreasing)
+            {
+                alpha += Time.deltaTime * blinkSpeed;
+                if (alpha >= 1.0f) // 最大透明度に達したら減少に切り替え
+                {
+                    alpha = 1.0f;
+                    isIncreasing = false;
+                }
+            }
+            else
+            {
+                alpha -= Time.deltaTime * blinkSpeed;
+                if (alpha <= 0.3f) // 最小透明度に達したら増加に切り替え
+                {
+                    alpha = 0.3f;
+                    isIncreasing = true;
+                }
+            }
+
+            // Imageの透明度を設定
+            Color color = buttonImage.color;
+            color.a = alpha;
+            buttonImage.color = color;
+        }
     }
 }
