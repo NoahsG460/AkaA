66adc0bbc4418a1dc49b8c7ea714926bcd4b40f2
NoahsG460 <xinyihouteng729@gmail.com>
2025-01-24 09:34:46 +0900

    .21

---
+++ Assets/PUN/PunConnector.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Punconnector : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start!");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Mastar Connected");
+        
+        Debug.Log("Join Room Start");
+        PhotonNetwork.JoinOrCreateRoom("room" , new RoomOptions() , TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Room Joined");
+    }
+}
